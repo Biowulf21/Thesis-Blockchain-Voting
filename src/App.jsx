7de8dd13e7a4becd85cd31434457892a6ff343a9
James Jilhaney <notail2111@gmail.com>
2023-02-03 16:19:29 +0800

    Working voting function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 import { ethers } from 'ethers';
 
+var contractInstance;
 function App() {
+  var oten;
   const [account, setAccount] = useState(null);
   const [candidate1, setCandidate1] = useState(null);
   const [candidate2, setCandidate2] = useState(null);
@@ -13,18 +15,18 @@ function App() {
 
   const {ethereum} = window;
 
-  useEffect(() => {
-    if (account !== null) {
-      connectToSmartContract();
-    }
+  // useEffect(() => {
+  //   if (account !== null) {
+  //     connectToSmartContract();
+  //   }
     
-  }, [account])
+  // }, [account])
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      connectToSmartContract();
-    }
-  }, [isLoggedIn])
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     connectToSmartContract();
+  //   }
+  // }, [isLoggedIn])
 
   useEffect(() => {}, [chosenCandidateState])
   
@@ -36,6 +38,7 @@ function App() {
       });
       setIsLoading(true);
       setAccount(accounts[0]);
+      connectToSmartContract();
       setIsLoading(false);
       setIsLoggedIn(true);
     }
@@ -43,7 +46,6 @@ function App() {
 
 
   
-  let contractInstance;
   async function connectToSmartContract(){
     const address = '0x7A7333d93C9Ba7a3c63dD3F12D94622E440dfa99';
     const ABI = [
@@ -143,12 +145,35 @@ function App() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     contractInstance = new ethers.Contract(address, ABI, signer);
-
     getCandidatesInfo()
   }
-  async function getCandidatesInfo(){
 
+  async function vote(){
+    if (chosenCandidateState == null){
+      alert("Please select a candidate");
+      return;
+    }
+
+    if (chosenCandidateState !== 1 && chosenCandidateState !== 2){
+      alert("Candidate ID not recognised.");
+      return;
+    }
+
+    try {
+      
+      const voteResult = await contractInstance.vote(chosenCandidateState);
+      console.log(voteResult);
+      alert("Voting Success");
+    } catch (error) {
+      console.log(error);
+      
+    }
     
+
+  }
+
+
+  async function getCandidatesInfo(){
       let candidateInfo = await contractInstance.candidates(1);
       setCandidate1(candidateInfo.name)
       candidateInfo = await contractInstance.candidates(2);
@@ -163,6 +188,8 @@ function App() {
     }
   }
 
+  
+
   return (
   <>
     {isLoggedIn === true && candidate1 !== null && candidate2 !== null ? (
@@ -170,29 +197,42 @@ function App() {
       <div className="main-container">
         <div className="candidate-container">
           <div className="candidate-img-div">
-            <img className='candidate-img' src="https://scontent.fcgm1-1.fna.fbcdn.net/v/t39.30808-6/324102820_2216538285216504_6974961729629479937_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5XrkS4oRcA0AX-hhqZg&_nc_ht=scontent.fcgm1-1.fna&oh=00_AfC7yPHG3RLcCjBmPd1MY0rG6cDKkiUcITmOmKaIKFUUpA&oe=63D79664"/>
+            <img className='candidate-img' src="https://images.pexels.com/photos/4427630/pexels-photo-4427630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
           </div>
           <div className="candidate-info">
           <input
-            checked={chosenCandidateState === 2 }
-            onChange={() => setChosenCandidate()}
+            className='candidate-checkbox'
+            checked={chosenCandidateState === 1 }
+            onChange={() => setChosenCandidate(1)}
             type='checkbox'
             >
             </input>
             {candidate1}
           </div>
-          <div className="vote-btn-container">
-            <button className='vote-btn'>oten</button>
-          </div>
-          
-
         </div>
-
+        <div className="candidate-container">
+          <div className="candidate-img-div">
+            <img className='candidate-img' src="https://images.pexels.com/photos/4427506/pexels-photo-4427506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          </div>
+          <div className="candidate-info">
+          <input
+            className='candidate-checkbox'
+            checked={chosenCandidateState === 2 }
+            onChange={() => setChosenCandidate(2)}
+            type='checkbox'
+            >
+            </input>
+          <p className='candidate-name-txt'>{candidate2}</p> 
+          </div>      
+        </div>
+          <div className="vote-btn-container">
+            <button onClick={vote} className='vote-btn'>Vote</button>
+          </div>
       </div>
     </>
     ) : isLoading === true? (<div>Loading...</div>) : (
-      <div>
-        <button onClick={connectToMetaMask}>Log in</button>
+      <div className='unauth-main-container'>
+        <button className="login-btn" onClick={connectToMetaMask}>Log in</button>
       </div>
     ) }
   </>
